6961a99b9a4ee305720e7ee30903fd20a2a2674b
Axel <axldotm@gmail.com>
2018-10-18 20:56:35 +0100

    plot every animals on one graph.

--- main.m
+++ main.m
@@ -1,3 +1,4 @@
+close all;
 fprintf('start...\n');
 fprintf('connecting to mongoDB...\n');
 javaaddpath('C:\Users\Axel\Documents\MATLAB\JAR\mongo-java-driver-3.8.2.jar')
@@ -9,7 +10,7 @@ fprintf('find collections...\n');
 colls = db.getCollectionNames().toArray();
 disp(colls);
 
-currColl = "_70091100056-3/31/13";
+currColl = "_70091100056-4/1/13";
 disp("currColl="+currColl);
 
 coll = db.getCollection(currColl).findOne().toString().toCharArray;
@@ -36,51 +37,66 @@ end
 if A == 1
     fprintf('v1 sensor plot...\n');
     animalArray = json.animals;
-    values = []; times = [];
+    rows = size(animalArray);
+    r = rows(1);
+    maxCols = 0;
+    names = [];
+        
+        names = strings(1,r);
+        figure(); hold on
         for i = 1:size(animalArray)
-            y = []; d = [];
+            times = [];
+            values = [];
             tagDataArray = animalArray(i).tagData;
-            name = tagDataArray(1).serial_number.x_numberLong;
+            names(i) = tagDataArray(1).serial_number.x_numberLong;
               for j = 1:size(tagDataArray)
                     tag = tagDataArray(j);
                     TF = contains(tag.time,'PM')
                     if TF == 1
                         time = datenum(tag.time, 'HH:MM:SS PM');
                         fprintf("PM\n"+time);
-                        d = [d, time]; 
                     end
                     if TF == 0
                         time = datenum(tag.time, 'HH:MM:SS AM');
                         fprintf("AM\n"+time)
-                        d = [d, time]; 
                     end
                     value = tag.first_sensor_value;
-                   
-                    y = [y, value];                    
+                    if value < 0
+                        value = 0;
+                    end
+                    values = [values, value];
+                    times = [times, time];
+                    
+                    if j > maxCols
+                        maxCols = j;
+                    end
               end
-              values = [values, y];
-              times = [times, d];
-              
-              %find the the size of the largest vector
-              max = 0;
-              for n = 1:size(values)
               
               
-              end
-                  
-            
+        %values = values(:,1:maxCols);
+        
+        N = size(animalArray);
+        W = floor(sqrt(N));
+        width = W(1)+1;
+        height = ceil(N/W);
+        
+        %y = values(i,1:maxCols);
+        plot(times, values);  
+        datetick('x','HH:MM') 
               
-              N = size(animalArray);
-              W = floor(sqrt(N));
-              width = W(1)+1;
-              height = ceil(N/W);
-              %subplot(width,height,i);
-              figure(i)
-              plot(d,y);              % plot the data,
-              %hold on;
-              datetick('x','HH:MM')   % give the a xaxis time label ticks..
-              title(name);
         end
+        hold off
+
+        
+              %figure(); hold on
+              %for m = 1:size(values,1)
+                  %subplot(width,height,i);
+                  %plot(times(m,:), values(m,:));              % plot the data,
+                  %datetick('x','HH:MM')   % give the a xaxis time label ticks..
+                  %title(names(m));
+              %end
+              %hold off
+              
 end
 
 function acceleration(input)
